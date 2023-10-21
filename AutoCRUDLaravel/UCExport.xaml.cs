e9a12b4e86298d66669de0a3fa03bf088900d99c
RafaelBento7 <rafaelcarreirabento7@gmail.com>
2023-10-21 20:26:31 +0100

    Create View in Preview and Preview Clicks

--- AutoCRUDLaravel/UCExport.xaml.cs
+++ AutoCRUDLaravel/UCExport.xaml.cs
@@ -1,25 +1,117 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace AutoCRUDLaravel {
     /// <summary>
     /// Interação lógica para UCExport.xam
     /// </summary>
     public partial class UCExport : UserControl {
+        private enum View {
+            MODEL,
+            CONTROLLER,
+            JAVASCRIPT,
+            INDEX,
+            CREATE,
+            SHOW,
+            EDIT
+        }
+
+        private View currentView;
+
         public UCExport(List<Column> columns) {
             InitializeComponent();
+            SetView(currentView);
         }
+
+        private void SetView(View currentView) {
+            this.currentView = currentView;
+
+            colorModel.Visibility = Visibility.Hidden;
+            colorController.Visibility = Visibility.Hidden;
+            colorJavaScript.Visibility = Visibility.Hidden;
+            colorIndex.Visibility = Visibility.Hidden;
+            colorCreate.Visibility = Visibility.Hidden;
+            colorShow.Visibility = Visibility.Hidden;
+            colorEdit.Visibility = Visibility.Hidden;
+
+            switch (currentView) {
+                case View.MODEL:
+                    colorModel.Visibility = Visibility.Visible;
+                    break;
+                case View.CONTROLLER:
+                    colorController.Visibility = Visibility.Visible;
+                    break;
+                case View.JAVASCRIPT:
+                    colorJavaScript.Visibility = Visibility.Visible;
+                    break;
+                case View.INDEX:
+                    colorIndex.Visibility = Visibility.Visible;
+                    break;
+                case View.CREATE:
+                    colorCreate.Visibility = Visibility.Visible;
+                    break;
+                case View.SHOW:
+                    colorShow.Visibility = Visibility.Visible;
+                    break;
+                case View.EDIT:
+                    colorEdit.Visibility = Visibility.Visible;
+                    break;
+            }
+        }
+
+        private void LoadPreview(View currentView) {
+            switch (currentView) {
+                case View.MODEL:
+                    break;
+                case View.CONTROLLER:
+                    break;
+                case View.JAVASCRIPT:
+                    break;
+                case View.INDEX:
+                    break;
+                case View.CREATE:
+                    break;
+                case View.SHOW:
+                    break;
+                case View.EDIT:
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        #region Right-Tab StackPanel Clicks
+
+        private void Model_PreviewMouseDown(object sender, MouseButtonEventArgs e) {
+            SetView(View.MODEL);
+        }
+
+        private void Controller_PreviewMouseDown(object sender, MouseButtonEventArgs e) {
+            SetView(View.CONTROLLER);
+        }
+
+        private void JavaScript_PreviewMouseDown(object sender, MouseButtonEventArgs e) {
+            SetView(View.JAVASCRIPT);
+        }
+
+        private void Index_PreviewMouseDown(object sender, MouseButtonEventArgs e) {
+            SetView(View.INDEX);
+        }
+
+        private void Create_PreviewMouseDown(object sender, MouseButtonEventArgs e) {
+            SetView(View.CREATE);
+        }
+
+        private void Show_PreviewMouseDown(object sender, MouseButtonEventArgs e) {
+            SetView(View.SHOW);
+        }
+
+        private void Edit_PreviewMouseDown(object sender, MouseButtonEventArgs e) {
+            SetView(View.EDIT);
+        }
+
+        #endregion
     }
 }
